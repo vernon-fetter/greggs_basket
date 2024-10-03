5f8a3a52e78a4491b9a106ad52af5bf1806a7ef1
Vern Fetter <38083819+vernon-fetter@users.noreply.github.com>
2024-10-03 15:09:59 +0200

    Implement BasketManager for managing basket state with add/remove functionality and price calculations.

--- lib/providers/basket_manager.dart
+++ lib/providers/basket_manager.dart
@@ -1,24 +1,53 @@
 import 'package:flutter/material.dart';
 import '../models/sausage_roll.dart';
+import '../services/storage_service.dart';
 
 class BasketManager extends ChangeNotifier {
-  final List<SausageRoll> _items = [];
+  final List<Map<String, dynamic>> _items = [];
+  final StorageService _storageService;
+  bool _isEatIn = false;
 
-  List<SausageRoll> get items => _items;
+  BasketManager(this._storageService) {
+    loadBasket();
+  }
 
-  // Calculate the total price of all items in the basket
-  double get totalPrice =>
-      _items.fold(0, (total, item) => total + item.eatOutPrice);
+  List<Map<String, dynamic>> get items => _items;
 
-  // Method to add an item to the basket
-  void addItem(SausageRoll item) {
-    _items.add(item);
+  bool get isEatIn => _isEatIn;
+
+  double get totalPrice {
+    return _items.fold(0, (total, item) {
+      double price = _isEatIn ? item['sausageRoll'].eatInPrice : item['sausageRoll'].eatOutPrice;
+      return total + (price * item['quantity']);
+    });
+  }
+
+  void toggleEatIn() {
+    _isEatIn = !_isEatIn;
     notifyListeners();
   }
 
-  // Clear the basket
+  void addItem(SausageRoll item, int quantity) {
+    _items.add({'sausageRoll': item, 'quantity': quantity});
+    _storageService.saveBasket(_items);
+    notifyListeners();
+  }
+
+  void removeItem(int index) {
+    _items.removeAt(index);
+    _storageService.saveBasket(_items);
+    notifyListeners();
+  }
+
   void clearBasket() {
     _items.clear();
+    _storageService.saveBasket(_items);
+    notifyListeners();
+  }
+
+  void loadBasket() async {
+    List<Map<String, dynamic>> savedItems = await _storageService.loadBasket();
+    _items.addAll(savedItems);
     notifyListeners();
   }
 }
