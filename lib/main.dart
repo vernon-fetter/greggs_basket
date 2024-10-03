f84a2ba2c6af2c359c6812548cded7b6d2c702b9
Vern Fetter <38083819+vernon-fetter@users.noreply.github.com>
2024-10-03 15:08:38 +0200

    Initialize app with BasketManager using StorageService for basket persistence.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/basket_manager.dart';
 import 'screens/sausage_roll_screen.dart';
+import 'services/storage_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => BasketManager(),
+      create: (context) => BasketManager(StorageService()),
       child: MaterialApp(
         title: 'Greggs Sausage Roll Basket',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
+        debugShowCheckedModeBanner: false,
         home: SausageRollScreen(),
       ),
     );
